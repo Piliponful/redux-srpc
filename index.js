69aab97071caee6213d9daa4709da3a8095e8aeb
piliponful <piliponful@gmail.com>
2020-04-04 22:29:20 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const createSrpcMiddleware = url => ({ dispatch, getState }) => (next) => (action) => {
+  if (action.type === 'SRPC_CALL') {
+    return window.fetch(url, {
+      method: 'post',
+      body: JSON.stringify(action.payload),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => response.json())
+      .then(payload => {
+        next({ type: `${action.payload.functionName}Result`, payload })
+      })
+      .catch(console.error)
+  }
+
+  return next(action)
+}
+
+export default createSrpcMiddleware
